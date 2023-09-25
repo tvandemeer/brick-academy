520b1b209613682a8830f4e3d730a9f8336d00e5
Thomas van de Meer <t.vandemeer@gmail.com>
2023-09-25 19:45:21 +0200

    cards

--- js/app.js
+++ js/app.js
@@ -2,18 +2,63 @@ const getUrl = 'https://rebrickable.com/api/v3/lego/parts/?page=';
 
 const page = 45;
 
-async function getParts(url = '') {
-  const response = await fetch(url, {
-    method: 'GET',
-    headers: {
-      Authorization: 'key bc4537b496eaab6056e0ce49fb54bc55',
-    },
-  });
-  return response.json();
-}
+window.onload = () => {
 
-getParts(getUrl + page.toString())
-  .then((data) => {
-    // console.log(data["results"]);
-    localStorage.setItem('parts', data.results);
-  });
+    const parts_list = document.getElementById('parts_list');
+
+    async function getParts(url = '') {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                Authorization: 'key bc4537b496eaab6056e0ce49fb54bc55',
+            },
+        });
+        return response.json();
+    }
+
+    getParts(getUrl + page.toString())
+        .then((data) => {
+            localStorage.setItem('parts', JSON.stringify(data));
+            data['results'].forEach((item) => {
+                createPartEl(item['part_num'], item['name']);
+            });
+            const link_prev = data['previous'];
+            const link_next = data['next'];
+            if (link_prev) {
+                const prevBtn = document.createElement('button');
+                const prevLink = document.createElement('a');
+                prevBtn.type = 'button';
+                prevBtn.classList.add('uk-button');
+                prevLink.href = link_prev;
+                prevLink.innerText = 'vorige';
+                prevBtn.appendChild(prevLink);
+                parts_list.appendChild(prevBtn);
+            }
+            if (link_next) {
+                const nextBtn = document.createElement('button');
+                const nextLink = document.createElement('a');
+                nextBtn.type = 'button';
+                nextBtn.classList.add('uk-button');
+                nextLink.href = link_next;
+                nextLink.innerText = 'volgende';
+                nextBtn.appendChild(nextLink);
+                parts_list.appendChild(nextBtn);
+            }
+        });
+
+    function createPartEl (part_num, name) {
+        const partDiv = document.createElement('div');
+        const numEl = document.createElement('p');
+        const nameEl = document.createElement('p');
+        partDiv.classList.add('uk-card');
+        partDiv.classList.add('uk-card-body');
+        partDiv.classList.add('uk-card-default');
+        partDiv.classList.add('uk-width-1-4@l');
+        partDiv.classList.add('uk-width-1-1@s');
+        numEl.innerText = part_num;
+        nameEl.innerText = name;
+        partDiv.appendChild(numEl);
+        partDiv.appendChild(nameEl);
+        parts_list.appendChild(partDiv);
+    }
+};
