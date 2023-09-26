ef57b621b25754a203fe9f23fe6d778c8963d168
Thomas van de Meer <t.vandemeer@gmail.com>
2023-09-26 17:06:04 +0200

    image placeholders

--- js/app.js
+++ js/app.js
@@ -18,9 +18,15 @@ async function getParts(url = '') {
 getParts(getUrl + '?page=' + page.toString())
     .then((data) => {
         sessionStorage.setItem('parts', JSON.stringify(data));
+        let part_nums = '?part_nums=';
         data['results'].forEach((item) => {
+            part_nums += item['part_num'].toString() + ',';
             createPartEl(item['part_num'], item['name']);
         });
+        getDetails(url = getUrl + part_nums)
+            .then((data) => {
+                sessionStorage.setItem('details', JSON.stringify(data));
+            });
         const link_prev = data['previous'];
         const link_next = data['next'];
         if (link_prev) {
@@ -46,30 +52,32 @@ getParts(getUrl + '?page=' + page.toString())
     });
 
 function createPartEl (part_num, name) {
-    //getDetails(url = getUrl + part_num.toString()); TOO MANY REQUESTS!!!
     const wrapDiv = document.createElement('div');
     const partDiv = document.createElement('div');
-    const headerDiv = document.createElement('div');
+    const imgDiv = document.createElement('div');
     const bodyDiv = document.createElement('div');
-    const footerDiv = document.createElement('div');
     const numEl = document.createElement('p');
     const nameEl = document.createElement('p');
     const priceEl = document.createElement('p');
+    const imgEl = document.createElement('img');
+    imgEl.id = part_num;
+    imgEl.src = "https://placehold.co/600x400";
     partDiv.classList.add('uk-card');
     partDiv.classList.add('uk-card-default');
-    headerDiv.classList.add('uk-card-header');
+    imgDiv.classList.add('uk-card-media-top');
     bodyDiv.classList.add('uk-card-body');
-    footerDiv.classList.add('uk-card-footer');
     nameEl.classList.add('uk-card-title');
+    priceEl.classList.add('uk-position-bottom');
+    priceEl.classList.add('price');
+    imgDiv.appendChild(imgEl);
+    partDiv.appendChild(imgDiv);
     numEl.innerText = part_num;
     nameEl.innerText = name;
     priceEl.innerText = '€' + (Math.random() * 5).toFixed(2);
-    headerDiv.appendChild(nameEl);
+    bodyDiv.appendChild(nameEl);
     bodyDiv.appendChild(numEl);
-    footerDiv.appendChild(priceEl);
-    partDiv.appendChild(headerDiv);
+    bodyDiv.appendChild(priceEl);
     partDiv.appendChild(bodyDiv);
-    partDiv.appendChild(footerDiv);
     wrapDiv.appendChild(partDiv);
     parts_list.appendChild(wrapDiv);
 }
@@ -84,10 +92,8 @@ async function getDetails(url = '') {
     return response.json();
 }
 
-//getDetails(url)
-    //.then((data) => {
-        ////sessionStorage.setItem(part_num.toString(), JSON.stringify(data));
-        ////data['results'].forEach((item) => {
-            ////createPartEl(item['part_num'], item['name']);
-        //console.log(data);
-    //});
+partDetails = JSON.parse(sessionStorage.getItem('details'));
+partDetails['results'].forEach((item) => {
+    console.log(item['part_img_url']);
+});
+console.log(Object.keys(partDetails['results']));
