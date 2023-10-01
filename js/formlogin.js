708ec54be6b56dfd6add4cd252298bd0a898b348
Thomas van de Meer <t.vandemeer@gmail.com>
2023-10-01 11:47:28 +0200

    login en storage, work in progress

--- js/formlogin.js
+++ js/formlogin.js
@@ -1,6 +1,6 @@
 class Klant {
-  constructor(klant_id, naam, admin, logged_in) {
-    this.klant_id = klant_id;
+  constructor(naam, admin, logged_in) {
+    // this.klant_id = klant_id;
     this.naam = naam;
     this.admin = admin;
     this.logged_in = logged_in;
@@ -14,72 +14,112 @@ class Mandje {
   }
 }
 
-if (localStorage.getItem('klanten')) {
-  const klanten = JSON.parse(localStorage.getItem('klanten'));
-  console.log(`klanten: ${klanten}`);
-} else {
-  console.log('geen klanten array in localStorage');
-}
-if (localStorage.getItem('winkelmandjes')) {
-  const winkelmandjes = JSON.parse(localStorage.getItem('winkelmandjes'));
-  console.log(`winkelmandjes: ${winkelmandjes}`);
-} else {
-  console.log('geen winkemandjes array in localStorage');
-}
-if (localStorage.getItem('artikelen')) {
-  const artikelen = JSON.parse(localStorage.getItem('artikelen'));
-  console.log(`artikelen: ${artikelen}`);
-} else {
-  console.log('geen artikelen array in localStorage');
-}
+// if (localStorage.getItem('klanten')) {
+// let klanten = JSON.parse(localStorage.getItem('klanten'));
+// console.log('klanten: ' + klanten);
+// } else {
+// console.log('geen klanten array in localStorage');
+// }
+// if (localStorage.getItem('winkelmandjes')) {
+// let winkelmandjes = JSON.parse(localStorage.getItem('winkelmandjes'));
+// console.log('winkelmandjes: ' + winkelmandjes);
+// } else {
+// console.log('geen winkemandjes array in localStorage');
+// }
+// if (localStorage.getItem('artikelen')) {
+// let artikelen = JSON.parse(localStorage.getItem('artikelen'));
+// console.log('artikelen: ' + artikelen);
+// } else {
+// console.log('geen artikelen array in localStorage');
+// }
 
 const loginButton = document.getElementById('button-login');
 loginButton.addEventListener('click', (event) => {
   event.preventDefault();
+  let klant;
   const naam = document.getElementById('input-naam').value;
   const admin = document.getElementById('check-admin').checked;
-  console.log(`naam: ${naam}`);
-  console.log(`admin: ${admin}`);
+  // console.log('naam: ' + naam);
+  // console.log('admin: ' + admin);
   if (naam) {
-    if (localStorage.getItem(`klant_${naam}`)) {
-      const klant = JSON.parse(localStorage.getItem(`klant_${naam}`));
-      klant.admin = admin;
-      klant.logged_in = true;
-      localStorage.setItem(klant.klant_id, JSON.stringify(klant));
-      const live_klant = klant;
-      sessionStorage.setItem('live_klant', JSON.stringify(live_klant));
-      console.log('klant gevonden in localStorage');
-    } else {
-      const klant = createKlant(naam, admin);
+    // if (localStorage.getItem('klant_' + naam)) {
+    if (localStorage.getItem('klanten')) {
+      const klanten = JSON.parse(localStorage.getItem('klanten'));
+      console.log(klanten);
+      let existing = 0;
+      klanten.forEach((item) => {
+        if (item.naam === naam) {
+          existing = 1;
+          klant = item;
+        }
+      });
+      console.log(existing);
       console.log(klant);
-      const live_klant = klant;
-      sessionStorage.setItem('live_klant', JSON.stringify(live_klant));
+      if (existing) {
+
+      } else {
+
+      }
     }
-    Swal.fire({
-      title: `Hallo ${naam}!`,
-      icon: 'success',
-      text: 'je bent ingelogd',
-      timer: 2000,
-      timerProgressBar: true,
-    });
-    const userLinks = document.querySelectorAll('.link-user');
-    userLinks.forEach((link) => {
-      link.style.color = '#0F7AE5';
-    });
   } else {
-    Swal.fire({
-      title: 'Vul een naam in',
-      icon: 'warning',
-      timer: 1500,
-      timerProgressBar: true,
-    });
+    console.log('geen naam');
   }
+
+  // if (klanten[naam]) {
+  // let klant = JSON.parse(localStorage.getItem('klant_' + naam));
+  // console.log(klanten[naam]);
+  // klanten[naam].admin = admin;
+  // klanten[naam].logged_in = true;
+  // let klant = klanten[naam];
+  // klant.admin = admin;
+  // klant.logged_in = true;
+  // localStorage.setItem(klant.klant_id, JSON.stringify(klant));
+  // localStorage.setItem('klanten', JSON.stringify(klanten));
+  // let live_klant = klant;
+  // sessionStorage.setItem('live_klant', JSON.stringify(live_klant));
+  // console.log('klant gevonden in localStorage');
+  // } else {
+  // let klant = createKlant(naam, admin);
+  // klanten.push(klant);
+  // localStorage.setItem('klanten', JSON.stringify(klanten));
+  // let live_klant = klant;
+  // sessionStorage.setItem('live_klant', JSON.stringify(live_klant));
+  // }
+  // } else {
+  // let klanten = [];
+  // let klant = createKlant(naam, admin);
+  // klanten.push(klant);
+  // localStorage.setItem('klanten', JSON.stringify(klanten));
+  // console.log(klant);
+  // let live_klant = klant;
+  // sessionStorage.setItem('live_klant', JSON.stringify(live_klant));
+  // }
+  // Swal.fire({
+  // title: "Hallo " + naam + "!",
+  // icon: "success",
+  // text: "je bent ingelogd",
+  // timer: 2000,
+  // timerProgressBar: true
+  // });
+  // const userLinks = document.querySelectorAll('.link-user');
+  // userLinks.forEach((link) => {
+  // link.style.color = "#0F7AE5";
+  // });
+  // } else {
+  // Swal.fire({
+  // title: "Vul een naam in",
+  // icon: "warning",
+  // timer: 1500,
+  // timerProgressBar: true
+  // });
+  // }
 });
 
 function createKlant(naam, is_admin) {
   const logged_in = true;
-  const klant = new Klant(`klant_${naam}`, naam, is_admin, logged_in);
-  localStorage.setItem(klant.klant_id, JSON.stringify(klant));
+  // const klant = new Klant('klant_' + naam, naam, is_admin, logged_in);
+  const klant = new Klant(naam, is_admin, logged_in);
+  // localStorage.setItem(klant.klant_id, JSON.stringify(klant));
   console.log('klant created');
   return klant;
 }
